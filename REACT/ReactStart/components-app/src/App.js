53d6d63c8561ff733c81a8781c843b946cde5dbf
JonasN05 <jonas.nigg@student.htldornbirn.at>
2023-12-13 12:17:59 +0100

    List

--- REACT/ReactStart/components-app/src/App.js
+++ REACT/ReactStart/components-app/src/App.js
@@ -1,14 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 import Button from './components/button/Button';
 import Card from './components/card/Card';
+import List from './components/list/List';
 
 function App() {
+  var array = ["Coffe", "Tea", "Bear"]
   return (
     <div className="App">
       <header className="App-header">
-        <Button name ="Sendar"/>
+        <div className="button">
+          <Button name ="Sendar"/>
+        </div>
         <Card img="https://www.w3schools.com/howto/img_avatar.png" name="John Doe" description="Architect & Engineer" />
+        <List data={array}/>
       </header>
 
     </div>
--- REACT/ReactStart/components-app/src/components/list/List.js
+++ REACT/ReactStart/components-app/src/components/list/List.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useLayoutEffect } from 'react'
+import styles from './List.module.css'
 
-export default function List() {
-  const createList=()=>{
-      
-  }
+export default function List({data}) {
+  console.log(data);
+  
+
   return (
-    <div>
-      
-    </div>
+      <div>
+        {data.map(item=>(<div className={styles.listElement}>{item} </div>))}    
+      </div>
   )
+       
 }
